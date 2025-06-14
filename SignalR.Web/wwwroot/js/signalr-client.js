94c11917d5e22fd8c22c28d00f866d9b55e2c20b
NihadRasulzada <nihadrs2005@gmail.com>
2025-06-14 07:08:06 +0400

    SendMessageToHubWithClient

--- SignalR.Web/wwwroot/js/signalr-client.js
+++ SignalR.Web/wwwroot/js/signalr-client.js
@@ -1,4 +1,6 @@
 ﻿$(document).ready(function () {
+    const broadcastMessageToAllClientHubMethodCall = "BroadcastMessageToAllClients"; 
+    const receiveMessageForAllClientClientMethodCall = "ReceiveMessageForAllClient";
     const connection = new signalR.HubConnectionBuilder()
         .withUrl("/examplehub")
         .configureLogging(signalR.LogLevel.Information)
@@ -14,4 +16,15 @@
     catch {
         setTimeout(() => start(), 5000);
     }
+
+    connection.on(receiveMessageForAllClientClientMethodCall, (message) => {
+        console.log("Received message: " + message);
+    })
+
+    $("#btn-send-message-all-client").click(function () {
+        const message = "Salam";
+
+        connection.invoke(broadcastMessageToAllClientHubMethodCall, message)
+            .catch(err => console.error("Error sending message: " + err));
+    })
 });

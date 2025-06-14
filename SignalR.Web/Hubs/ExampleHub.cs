0592a03f27c301b1e827bdb6eb22ba12266078a6
NihadRasulzada <nihadrs2005@gmail.com>
2025-06-14 06:48:01 +0400

    FirstClientHubConnection

--- SignalR.Web/Hubs/ExampleHub.cs
+++ SignalR.Web/Hubs/ExampleHub.cs
@@ -4,5 +4,9 @@ namespace SignalR.Web.Hubs
 {
     public class ExampleHub : Hub
     {
+        public async Task BroadcastMessageToAllClients(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessageForAllClient", message);
+        }
     }
 }
---
+++ SignalR.Web/wwwroot/js/signalr-client.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    const connection = new signalR.HubConnectionBuilder()
+        .withUrl("/examplehub")
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+
+    function start() {
+        connection.start().then(() => console.log("Connection started"));
+    }
+
+    try {
+        start();
+    }
+    catch {
+        setTimeout(() => start(), 5000);
+    }
+});
